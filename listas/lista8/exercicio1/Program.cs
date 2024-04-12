31e38ef618632cad33f5d744892c51135bc8097d
MarcusViniciusDev02 <m.v.paixaoo@gmail.com>
2024-04-12 20:25:50 -0300

    lista 8

---
+++ listas/lista8/exercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+struct Pizza
+{
+    //id, sabor e preço;
+    public int id;
+    public string sabor;
+    public double preco;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pizza pizza;
+
+        pizza.id = Convert.ToInt32(Console.ReadLine());
+        pizza.sabor = Console.ReadLine();
+        pizza.preco = Convert.ToDouble(Console.ReadLine());
+
+        System.Console.WriteLine($"{pizza.id}\n{pizza.sabor}\n{pizza.preco}");
+
+
+    }
+}
---
+++ listas/lista8/exercicio2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.Common;
+
+/*
+Sua tarefa é construir uma vitrine de produtos. Para isso, seu coordenador pediu para que você criasse um programa que contenha uma struct Produto, com os campos id (inteiro), nome (string), preco (double) e disponivelEmEstoque (bool).
+Após criada a struct, você deve criar um vetor de produtos, de tamanho 4 e preenchê-lo com dados recebidos via teclado.
+Ao final, seu programa deve exibir na vitrine o nome e o preço dos produtos que estão disponíveis em estoque.
+*/
+
+struct Produto
+{
+    public int id;
+    public string nome;
+    public double preco;
+    public bool disponivelEmEstoque;
+
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int TAMANHO_VETOR = 4;
+        Produto[] produto = new Produto[TAMANHO_VETOR];
+
+        for (int i = 0; i < produto.Length; i++)
+        {
+            produto[i].id = Convert.ToInt32(Console.ReadLine());
+            produto[i].nome = Console.ReadLine();
+            produto[i].preco = Convert.ToDouble(Console.ReadLine());
+            produto[i].disponivelEmEstoque = Convert.ToBoolean(Console.ReadLine());
+        }
+
+        for (int i = 0; i < produto.Length; i++)
+        {
+            if (produto[i].disponivelEmEstoque == true)
+            {
+                string precoComDuasCasas = produto[i].preco.ToString("F2");
+                System.Console.WriteLine(produto[i].nome);
+                System.Console.WriteLine(precoComDuasCasas);
+            }
+        }
+
+    }
+}
